91dcc7e7bdd427b33d733ff3bd257d62e1cc7e7d
Rohan <rohankumar2304@gmail.com>
2025-11-12 02:38:04 +0000

    editted stack.ts

--- stack.ts
+++ stack.ts
@@ -1,20 +1,20 @@
 // File: stack.ts
 
-// import { defineStackApp } from "@stackframe/stack";
-// import { StackNeon } from "@stackframe/neon";
+import { defineStackApp } from "@stackframe/stack";
+import { StackNeon } from "@stackframe/neon";
 
-// export const stack = defineStackApp({
-//   // Use the Neon database adapter
-//   database: StackNeon({
-//     // This will automatically read from your .env file
-//     connectionString: process.env.DATABASE_URL!,
-//   }),
-//   // Add any other config you need here
-// });
-
-import "server-only";
-import { StackServerApp } from "@stackframe/stack";
-
-export const stackServerApp = new StackServerApp({
-  tokenStore: "nextjs-cookie", // storing auth tokens in cookies
+export const stack = defineStackApp({
+  // Use the Neon database adapter
+  database: StackNeon({
+    // This will automatically read from your .env file
+    connectionString: process.env.DATABASE_URL!,
+  }),
+  // Add any other config you need here
 });
+
+// import "server-only";
+// import { StackServerApp } from "@stackframe/stack";
+
+// export const stackServerApp = new StackServerApp({
+//   tokenStore: "nextjs-cookie", // storing auth tokens in cookies
+// });
